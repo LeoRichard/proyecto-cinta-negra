7cf36f7f6383410f7ece4fbad3a31934573e3c6c
Richard Leo M <leo.richard2@gmail.com>
2019-10-24 15:46:54 -0600

    Configuration of .env and index development

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import { ApolloServer } from 'apollo-server';
-
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
+import mongoose from 'mongoose';
 
+const url = process.env.DATA_BASE;
+
+mongoose.connect(
+  url,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+const mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, "Error de conexion!"));
+mongoDB.on('open', () => console.log('Bd conectada'));
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
